986c960a3d5f6e853ddbd38281d029910b3790c1
Garett Petersen <garettpetersen@garetts-mbp.attlocal.net>
2022-05-01 20:51:02 -0700

    removing log

--- src/components/privateRoute.js
+++ src/components/privateRoute.js
@@ -3,7 +3,6 @@ import { navigate } from 'gatsby';
 import { isLoggedIn } from '../services/auth';
 
 const PrivateRoute = ({ component: Component, location, ...rest }) => {
-  console.log(location);
   if (!isLoggedIn() && location.pathname !== '/login') {
     navigate('/login');
     return null;
