4efdcaa362891cf492b897b740226479a3576d63
Garett Petersen <garettpetersen@garetts-mbp.attlocal.net>
2022-05-07 13:26:49 -0700

    updating-public-gallery

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './gallery.css';
 import { graphql, StaticQuery } from 'gatsby';
-import Image from 'gatsby-image';
 import styled from 'styled-components';
 import Lightbox from 'react-image-lightbox';
 import 'react-image-lightbox/style.css';
@@ -20,8 +19,8 @@ function Gallery(props) {
   const [imageLength, setImageLength] = React.useState(0);
 
   const open = ({ data, index }, e) => {
-    const imageLength = data.allS3ImageAsset.edges.length;
-    setImages(data.allS3ImageAsset.edges);
+    const imageLength = data.allS3Image.edges.length;
+    setImages(data.allS3Image.edges);
     setImageLength(imageLength);
     setShowBox(true);
     setPhotoIndex(index);
@@ -31,39 +30,35 @@ function Gallery(props) {
     <StaticQuery
       query={graphql`
       {
-        allS3ImageAsset(filter: { Key: { regex: "/public/g" } }) {
-          edges {
-            node {
-              Key
-              childImageSharp {
-                fluid(quality: 100) {
-                  ...GatsbyImageSharpFluid
+        allS3Image(filter: { Key: { regex: "/public/g" } }){
+            edges {
+                node {
+                  Key
+                  Url
                 }
               }
-            }
-          }
         }
       }
     `}
       render={data => (
         <div>
           <div className="image-grid">
-            {data.allS3ImageAsset.edges.map(({ node }, index) => (
+            {data.allS3Image.edges.map(({ node }, index) => (
               <ImageItem
                 className="image-item"
                 id={node.Key}
                 data-foo={index}
                 key={`Image-${index}`}
-                src={node.childImageSharp.fluid.src}
+                src={node.Url}
                 onClick={(e) => open({ data, index }, e)}
               >
-                <Image
-                  fluid={node.childImageSharp.fluid}
+                <img
+                  src={node.Url}
                   style={{
                     width: '100%',
-                    height: '100%',
+                    height: '600px',
                     maxHeight: '600px',
-                    objectFit: 'contain',
+                    objectFit: 'cover',
                   }}
                 />
               </ImageItem>
@@ -74,9 +69,9 @@ function Gallery(props) {
             {showBox && (
             <Lightbox
               onCloseRequest={() => setShowBox(false)}
-              mainSrc={images[photoIndex].node.childImageSharp.fluid.src}
-              nextSrc={images[(photoIndex + 1) % imageLength].node.childImageSharp.fluid.src}
-              prevSrc={images[(photoIndex + imageLength - 1) % imageLength].node.childImageSharp.fluid.src}
+              mainSrc={images[photoIndex].node.Url}
+              nextSrc={images[(photoIndex + 1) % imageLength].node.Url}
+              prevSrc={images[(photoIndex + imageLength - 1) % imageLength].node.Url}
               onMovePrevRequest={() => setPhotoIndex(
                 (photoIndex + imageLength - 1) % imageLength,
               )}
