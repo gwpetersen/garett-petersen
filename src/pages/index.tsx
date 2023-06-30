aa79e80eeebe00abc8be95e0d1f777844e12cb6b
gwpetersen <garettpetersencpp@gmail.com>
2023-06-29 18:22:09 -0700

    adding margin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,7 +48,7 @@ const PostCardHover = styled.div`
 const PostCard = styled(Card)`
 border-radius: 4px;
 background: #fff;
-margin: 10px;
+margin: 8px;
 box-shadow: 0 6px 10px rgba(0,0,0,.08), 0 0 6px rgba(0,0,0,.05);
 transition: .3s transform cubic-bezier(.155,1.105,.295,1.12),.3s box-shadow,.3s -webkit-transform cubic-bezier(.155,1.105,.295,1.12);
 padding: 14px 80px 18px 36px;
